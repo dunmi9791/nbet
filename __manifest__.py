8bc559e2d80d93f962f639b01f8d1332568890cb
dunmi9791 <dominic.anyanna@gmail.com>
2021-11-12 09:23:35 +0100

    dashboard

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail', 'account', 'om_account_budget', 'board'],
+    'depends': ['base', 'mail', 'account', 'om_account_budget', 'board', 'om_account_asset'],
 
     # always loaded
     'data': [
